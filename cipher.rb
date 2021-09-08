d5df18c90d560db86c064ed89f83b6ff9d0980f4
MandyWade0312 <amanda.wade@kin.com>
2021-09-08 12:30:24 -0400

    Asks the user the questions needed

--- cipher.rb
+++ cipher.rb
@@ -1,4 +1,15 @@
 def cipher(string, num)
-
+    
 
 end
+
+puts "Please enter your sentence"
+string = gets.chomp
+puts "\n"
+puts "Please choose a number between 1 and 9"
+num = gets.chomp.to_i
+puts "\n"
+puts "Converting....."
+puts "\n"
+
+cipher(string, num)
